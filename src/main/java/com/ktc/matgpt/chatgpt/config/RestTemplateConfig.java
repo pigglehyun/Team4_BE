874eb4dbebd2c7aab04b4b060339197f3b5e2704
이승현 <dltmdgus0661@naver.com>
2023-11-06 12:31:03 +0000

    chatGPT proxy config

--- src/main/java/com/ktc/matgpt/chatgpt/config/RestTemplateConfig.java
+++ src/main/java/com/ktc/matgpt/chatgpt/config/RestTemplateConfig.java
@@ -3,6 +3,7 @@ package com.ktc.matgpt.chatgpt.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
 
+@Profile(value = {"deploy"})
 @Configuration
 public class RestTemplateConfig {
 
